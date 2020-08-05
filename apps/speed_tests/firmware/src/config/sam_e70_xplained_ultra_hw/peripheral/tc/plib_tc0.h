5ed92856ae67e5c51acd69c908b11709a25f8705
Martin Bowman <martin.bowman@microchip.com>
2020-08-05 11:16:20 -0700

    MH3-38782 DFT Complete Pass

--- apps/speed_tests/firmware/src/config/sam_e70_xplained_ultra_hw/peripheral/tc/plib_tc0.h
+++ apps/speed_tests/firmware/src/config/sam_e70_xplained_ultra_hw/peripheral/tc/plib_tc0.h
@@ -119,10 +119,8 @@ void TC0_CH0_TimerCallbackRegister(TC_TIMER_CALLBACK callback, uintptr_t context
  
 
 
-#ifdef __cplusplus // Provide C++ Compatibility
-}
-#endif
-
 #endif //PLIB_TC0_H
 
-/* End of File */
+/**
+ End of File
+*/
