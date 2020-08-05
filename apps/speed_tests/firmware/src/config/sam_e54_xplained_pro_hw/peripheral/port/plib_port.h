9c3f9d39dac08c2338818dcbf128f2a98a9beddf
Martin Bowman <martin.bowman@microchip.com>
2020-08-05 11:32:27 -0700

    MH3-38784 Completed Pass

--- apps/speed_tests/firmware/src/config/sam_e54_xplained_pro_hw/peripheral/port/plib_port.h
+++ apps/speed_tests/firmware/src/config/sam_e54_xplained_pro_hw/peripheral/port/plib_port.h
@@ -1024,7 +1024,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Configures the selected IO pins of a group as input.
+    Confgiures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -1063,7 +1063,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Configures the selected IO pins of a group as output.
+    Confgiures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
